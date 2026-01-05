2a79b4ea25acb628d2cb8d95fc0cb23857ac5aa0
samarkun <samarkun4@gmail.com>
2026-01-05 13:52:36 -0500

    wallet-adapter

--- wallet-adapter/src/App.jsx
+++ wallet-adapter/src/App.jsx
@@ -1,14 +1,9 @@
-import React, { FC, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
   WalletModalProvider,
   WalletDisconnectButton,
   WalletMultiButton,
-  WalletConnectButton
 } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
